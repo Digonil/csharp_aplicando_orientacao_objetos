36c698d83b35c1c642adc58b67fa283cd30359af
Diego Niles <diego.niles@hotmail.com>
2023-07-15 10:31:24 -0300

    Adicionado aula 01

---
+++ screen_sound_oop/Musica.cs
@@ -0,0 +1,27 @@
+﻿class Musica 
+{
+   public string nome;
+   public string artista;
+   public int duracao;
+   public bool disponivel;
+
+    public void ExibirFichaTecnica() 
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel) 
+        {
+            Console.WriteLine($"Dísponivel no plano");
+        } else 
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+            
+    }
+
+    public void ExibirNomeMusicaEArtista() 
+    {
+        Console.WriteLine($"Nome: {nome} artista: {artista}");
+    }
+}
